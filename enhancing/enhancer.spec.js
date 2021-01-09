90b3f367feff897e9b065974013aaf6db182c123
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-01-09 10:36:14 -0800

    feat(enhancer): implement unit test
    added unit test to all functions

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,8 +1,87 @@
-const enhancer = require("./enhancer");
+const { get, fail, repair, success } = require("./enhancer");
 
+describe('player results', () => {
+   it("repairs durability", () => {
+      expect(repair({
+         name: "Player One",
+         durability: 50,
+         enhancement: 10
+      })).toEqual({
+         name: "Player One",
+         durability: 100,
+         enhancement: 10
+      });
+   });
 
-describe('initial test', () => {
-   it("it adds ", () => {
-      expect(2 + 2).toBe(4);
+   it("success", () => {
+      expect(success({
+         name: "Player One",
+         durability: 50,
+         enhancement: 10
+      })).toEqual({
+         name: "Player One",
+         durability: 50,
+         enhancement: 11
+      });
+   });
+
+   it("fails if enhancenment less than 15", () => {
+      expect(fail({
+         name: "Player One",
+         durability: 50,
+         enhancement: 14
+      })).toEqual({
+         name: "Player One",
+         durability: 45,
+         enhancement: 14
+      })
    })
+
+   it("fails if enhancenment is greater or equal to 15", () => {
+      expect(fail({
+         name: "Player One",
+         durability: 50,
+         enhancement: 15
+      })).toEqual({
+         name: "Player One",
+         durability: 40,
+         enhancement: 15
+      });
+   });
+
+   it("fails if enhancenment is greater than 16", () => {
+      expect(fail({
+         name: "Player One",
+         durability: 50,
+         enhancement: 19
+      })).toEqual({
+         name: "Player One",
+         durability: 40,
+         enhancement: 18
+      });
+   });
+
+   it("overwrittes name method", () => {
+      expect(get({
+         name: "Player One",
+         durability: 40,
+         enhancement: 18
+      })).toEqual({
+         name: "[+18] Iron Sword",
+         durability: 40,
+         enhancement: 18
+      });
+   });
+
+   it("leaves name as it is", () => {
+      expect(get({
+         name: "Player One",
+         durability: 40,
+         enhancement: 0
+      })).toEqual({
+         name: "Player One",
+         durability: 40,
+         enhancement: 0
+      });
+   });
 })
